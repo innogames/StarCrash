2dde307587d7a87821d965513eaec242fc0e969b
luca.hofmann <luca.hofmann@innogames.de>
2014-02-15 13:09:32 +0100

    Improved laser effects

--- source/htdocs/js/animations/LaserAnimation.js
+++ source/htdocs/js/animations/LaserAnimation.js
@@ -12,9 +12,9 @@ define(["THREE", "config"], function(THREE, config) {
 		this._durration = 200;
 
 		this._beamStartPosition = new THREE.Vector3(
-			config.player.graphics.weaponOffset.x + pStartPosition.x,
-			config.player.graphics.weaponOffset.y + pStartPosition.y,
-			config.player.graphics.weaponOffset.z + pStartPosition.z
+			config.player.graphics.weaponOffset.x,
+			config.player.graphics.weaponOffset.y,
+			config.player.graphics.weaponOffset.z
 		);
 
 		var cylinderGeometry = new THREE.CylinderGeometry(0.4, 0.4, this._beamLength, 20, 2, false);
@@ -25,12 +25,14 @@ define(["THREE", "config"], function(THREE, config) {
 		var sphereGeometry = new THREE.SphereGeometry(1);
 
 		this._laserBeam = new THREE.Mesh(cylinderGeometry, new THREE.MeshBasicMaterial({color: 0xFFAA22	}));
+		this._laserBeam.position = this._beamStartPosition;
 		this._laserShock = new THREE.Mesh(sphereGeometry, new THREE.MeshBasicMaterial({color: 0xFFAA00}));
+		this._laserShock.position = this._beamStartPosition.clone();
 
 		this.position.set(
-			this._beamStartPosition.x,
-			this._beamStartPosition.y,
-			this._beamStartPosition.z
+			pStartPosition.x,
+			pStartPosition.y,
+			pStartPosition.z
 		);
 
 		this.add(this._laserBeam);
@@ -40,23 +42,18 @@ define(["THREE", "config"], function(THREE, config) {
 
 		this.light = graphics.laserBeamLight;
 		this.light.distance = 100;
+
+		// add the start position for the light because it can not be added to this object3d
 		this.light.position.set(
-			this._beamStartPosition.x,
-			this._beamStartPosition.y,
-			this._beamStartPosition.z
+			this._beamStartPosition.x + pStartPosition.x,
+			this._beamStartPosition.y + pStartPosition.y,
+			this._beamStartPosition.z + pStartPosition.x
 		);
 
 
 		this._callback = function() {
 			this.light.intensity = 0;
 		};
-
-
-
-
-
-		//this.rotateOnAxis(new THREE.Vector3(0, 1 ,0), pDirection.y);
-		//this.rotateOnAxis(new THREE.Vector3(0, 0 ,1), Math.PI / 2);
 
 	};
 
@@ -90,13 +87,13 @@ define(["THREE", "config"], function(THREE, config) {
 	};
 
 	LaserAnimation.prototype.applyAnimationProgress = function(animationProgress) {
+		var invertedProgress = 1 - animationProgress;
+
 		this._laserBeam.position.z = this._beamStartPosition.z +  animationProgress * -1000;
 		this.light.position.z = this._beamStartPosition.z + animationProgress * -1000;
-		this.light.intensity = animationProgress * 4;
+		this.light.intensity = invertedProgress * 5;
 
-		var shockProgress = 1 - animationProgress * 2;
-		if (shockProgress < 0)  shockProgress = 0;
-		this._laserShock.scale.set(shockProgress, shockProgress, shockProgress);
+		this._laserShock.scale.set(invertedProgress, invertedProgress, invertedProgress);
 
 	};
 
