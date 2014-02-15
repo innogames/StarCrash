8d7d87d4e399f0ac66af1236d3f83cb5e1637e34
luca.hofmann <luca.hofmann@innogames.de>
2014-02-15 14:55:35 +0100

    Improved laser effects

--- source/htdocs/js/animations/LaserAnimation.js
+++ source/htdocs/js/animations/LaserAnimation.js
@@ -6,7 +6,7 @@ define(["THREE", "config"], function(THREE, config) {
 		this._direction = pDirection;
 		this._callback = pCallback;
 
-		this._beamLength = 100;
+		this._beamLength = 10000;
 
 		this._startTime = new Date().getTime();
 		this._durration = 200;
@@ -17,12 +17,12 @@ define(["THREE", "config"], function(THREE, config) {
 			config.player.graphics.weaponOffset.z
 		);
 
-		var cylinderGeometry = new THREE.CylinderGeometry(0.4, 0.4, this._beamLength, 20, 2, false);
+		var cylinderGeometry = new THREE.CylinderGeometry(0.2, 0.2, this._beamLength, 20, 2, false);
 		cylinderGeometry.applyMatrix( new THREE.Matrix4().makeRotationX( Math.PI / 2 ) );
 		cylinderGeometry.applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, -(this._beamLength / 2)));
 		cylinderGeometry.verticesNeedUpdate = true;
 
-		var sphereGeometry = new THREE.SphereGeometry(1, 10, 10);
+		var sphereGeometry = new THREE.SphereGeometry(0.8, 10, 10);
 
 		this._laserMaterial = new THREE.MeshBasicMaterial({ transparent: true, color: 0xFFAA22	});
 		this._torusMaterial = new THREE.MeshBasicMaterial({ transparent: true, color: 0xFFAA22	});
@@ -39,23 +39,37 @@ define(["THREE", "config"], function(THREE, config) {
 		);
 
 		this._torusObjects = [];
-		var torusGeometry = new THREE.TorusGeometry(0.4, 0.05, 2, 20);
-		for (var i = 0; i < 20; i++) {
+		var torusGeometry = new THREE.TorusGeometry(0.2, 0.05, 2, 20);
+		for (var i = 0; i < 100; i++) {
 			var torusMesh = new THREE.Mesh(torusGeometry, this._torusMaterial);
 			torusMesh.position.set(
 				this._beamStartPosition.x,
 				this._beamStartPosition.y,
-				this._beamStartPosition.z + (i * -10)
+				this._beamStartPosition.z + (i * -1)
 			);
+			torusMesh.fadeToX = (Math.random() - 0.5) * 0.1;
+			torusMesh.fadeToY = (Math.random() - 0.5) * 0.1;
+
 			this._torusObjects.push(torusMesh);
 			this.add(torusMesh);
 		}
 
+		this._lightTarget = new THREE.Object3D();
+
+		this._lightTarget.position.set(
+			this._beamStartPosition.x,
+			this._beamStartPosition.y,
+			this._beamStartPosition.z
+		);
+
 		this.add(this._laserBeam);
 		this.add(this._laserShock);
+		this.add(this._lightTarget);
 
 
 		this.rotation.y = pDirection.y;
+
+
 
 		this.light = graphics.laserBeamLight;
 		this.light.distance = 100;
@@ -99,22 +113,20 @@ define(["THREE", "config"], function(THREE, config) {
 		var self = this;
 		var invertedProgress = 1 - animationProgress;
 
-		this._laserBeam.position.z = this._beamStartPosition.z +  animationProgress * -1000;
-
 		this._laserMaterial.opacity = invertedProgress;
-		this._torusMaterial.opacity = invertedProgress * 0.1;
+		this._torusMaterial.opacity = invertedProgress * 0.2;
 
 		for (var i = 0; i < this._torusObjects.length; i++) {
-			this._torusObjects[i].scale.set(1 + animationProgress * 3 / (i * 0.5), 1 + animationProgress * 3 / (i * 0.5), 1);
+			this._torusObjects[i].scale.set(1 + animationProgress * 5 / (i * 0.5), 1 + animationProgress * 5 / (i * 0.5), 1);
+			this._torusObjects[i].position.x += this._torusObjects[i].fadeToX;
+			this._torusObjects[i].position.y += this._torusObjects[i].fadeToY;
 		}
 
-
-
-
 		// getting the absolute position of the beam for the light. (because the light can not be added to this object3)
+		this._lightTarget.position.z = this._beamStartPosition.z +  animationProgress * -1000;
 		this.updateMatrixWorld();
 		var absolutePosition = new THREE.Vector3();
-		absolutePosition.getPositionFromMatrix( self._laserBeam.matrixWorld);
+		absolutePosition.getPositionFromMatrix( self._lightTarget.matrixWorld);
 
 		this.light.position = absolutePosition;
 		this.light.intensity = invertedProgress * 5;
