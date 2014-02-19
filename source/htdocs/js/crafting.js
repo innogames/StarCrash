44cf202d82872c50230e0754baf7fbcddeb447af
julia-r <julia.rienitz@innogames.de>
2014-02-19 17:07:16 +0100

    redid crafting
    uses webSQL now, improved icons

--- source/htdocs/js/crafting.js
+++ source/htdocs/js/crafting.js
@@ -1,139 +1,308 @@
-var inventory, recipes;
-
-
 function init() {
 
-	read_json();
-
+	// Init buttons
 	$("#test").click(function(){
-		addItem("I5");
-		listInventory();
-		listRecipes();
+		crafting.webdb.addToInventory(11, 1, true);
 	});
 	$("#test1").click(function(){
-		removeItem("I5", 1);
-		listInventory();
-		listRecipes();
+		crafting.webdb.removeFromInventory(11, 1, true);
 	});
+
+	// Init Database
+	crafting.webdb.open();
+	crafting.webdb.createTable();
+	crafting.webdb.flushDB();
+
+	crafting.webdb.addItems();
+	crafting.webdb.addInventoryItems();
+	crafting.webdb.addRecipes();
+
+	crafting.webdb.getAllItems();
+	crafting.webdb.getMyItems();
+	crafting.webdb.getRecipes();
 }
 
-function read_json() {
 
-	// read Item list
-	 $.getJSON("items.json", function(myItemsData) {
 
-		inventory = myItemsData["inventory"];
-		listInventory();
-	});
+/* Local Storage DB Test */
 
-	// read recipes
-	$.getJSON("recipes.json", function(recipeData) {
+var crafting = {};
+crafting.webdb = {};
 
-		recipes = recipeData["recipes"];
-		listRecipes();
-	});
-}
+crafting.webdb.db = null;
 
-function listInventory(){
+crafting.webdb.open = function() {
+  var dbSize = 5 * 1024 * 1024; // 5MB
+  crafting.webdb.db = openDatabase("Crafting", "1", "Crafting Items", dbSize);
+};
 
-	// make sure list is empty
-	$("#inventory").html("");
+crafting.webdb.onError = function(tx, e) {
+  alert("There has been an error: " + e.message);
+};
 
-	$.each(inventory, function(i) {
+crafting.webdb.onSuccess = function(tx, r) {
+  	console.log("Success");
+};
 
-		if(inventory[i].numInInventory > 0){
-			$("#inventory").append('<li><i class="icon-' + inventory[i].icon + ' icon-3x"><span class="num">' + inventory[i].numInInventory + '</span><span class="name">' + inventory[i].name + '</span></i></li>');
+crafting.webdb.createTable = function() {
+
+	crafting.webdb.db.transaction(function(tx) {
+		tx.executeSql("CREATE TABLE IF NOT EXISTS " +
+					  "item(ID INTEGER PRIMARY KEY, name TEXT, class TEXT, dmgMin INTEGER, dmgMax INTEGER)", []);
+		tx.executeSql("CREATE TABLE IF NOT EXISTS " +
+					  "inventory(itemID INTEGER PRIMARY KEY, number INTEGER)", []);
+		tx.executeSql("CREATE TABLE IF NOT EXISTS " +
+					  "recipes(resultID INTEGER PRIMARY KEY, component1ID INTEGER, component2ID INTEGER, component3ID INTEGER)", []);
+  });
+};
+
+crafting.webdb.flushDB = function(id) {
+
+  crafting.webdb.db.transaction(function(tx){
+
+    tx.executeSql("DELETE FROM item",		[], crafting.webdb.onSuccess, crafting.webdb.onError);
+	tx.executeSql("DELETE FROM inventory",	[], crafting.webdb.onSuccess, crafting.webdb.onError);
+	tx.executeSql("DELETE FROM recipes",	[], crafting.webdb.onSuccess, crafting.webdb.onError);
+
+  });
+};
+
+var flushScreen = function(){
+
+	console.log("flush!");
+
+  	crafting.webdb.getAllItems();
+	crafting.webdb.getMyItems();
+	crafting.webdb.getRecipes();
+};
+
+
+/* -------------  Fill Database with values ---------------------------- */
+
+crafting.webdb.addItems = function() {
+
+	crafting.webdb.db.transaction(function(tx){
+		for (var i in items) {
+			tx.executeSql("INSERT INTO item(ID, name, class, dmgMin, dmgMax) VALUES (?,?,?,?,?)",
+				[items[i][0], items[i][1], items[i][2], items[i][3], items[i][4] ],
+				crafting.webdb.onSuccess, crafting.webdb.onError);
 		}
 	});
-}
+};
 
-function listRecipes(){
+crafting.webdb.addInventoryItems = function() {
 
-	// make sure list is empty
-	$("#recipes").html("");
+	crafting.webdb.db.transaction(function(tx){
+		for (var i in myItems) {
+			tx.executeSql("INSERT INTO inventory(itemID, number) VALUES (?,?)",
+				[myItems[i][0], myItems[i][1] ],
+				crafting.webdb.onSuccess, crafting.webdb.onError);
+		}
+	});
+};
 
-	$.each(recipes, function(i) {
-		var recipe = this;
-		var recipeHTML;
-		var craftable = true;
-		recipeHTML = "<h3>" + recipe.name + ":</h3><ul>";
+crafting.webdb.addRecipes = function() {
 
-		$.each(recipe["needed"], function(j) {
+	crafting.webdb.db.transaction(function(tx){
+		for (var i in recipes) {
+			tx.executeSql("INSERT INTO recipes(resultID, component1ID, component2ID, component3ID) VALUES (?,?,?,?)",
+				[recipes[i][0], recipes[i][1], recipes[i][2], recipes[i][3] ],
+				crafting.webdb.onSuccess, crafting.webdb.onError);
+		}
+	});
+};
 
-			var itemId = (recipe["needed"][j]["id"]);
-			var itemNum = (recipe["needed"][j]["num"]);
-			var itemIcon = inventory[itemId].icon;
-			var itemName = inventory[itemId].name;
-			var itemAvaiable = "available";
+/* Add Item to inventory */
 
-			if(inventory[itemId].numInInventory < itemNum){
-				itemAvaiable = "notAvailable";
-				craftable = false;
+crafting.webdb.addToInventory = function(itemID, num, flush) {
+
+	crafting.webdb.db.transaction(function(tx) {
+
+		tx.executeSql("INSERT OR IGNORE INTO inventory VALUES (?, 0); ", [itemID], crafting.webdb.onSuccess ,crafting.webdb.onError);
+		tx.executeSql("UPDATE inventory SET number = number + ? WHERE itemID = ?", [num, itemID], function (tx, results) {
+
+			crafting.webdb.onSuccess;
+
+			if(flush){
+				flushScreen();
+			}
+			console.log("update");
+
+		},crafting.webdb.onError);
+  });
+};
+
+
+/* remove item from inventory */
+crafting.webdb.removeFromInventory = function(itemID, num, flush) {
+
+	crafting.webdb.db.transaction(function(tx) {
+
+		// Check if there are enough items available
+		tx.executeSql("SELECT number FROM inventory WHERE itemID = ?", [itemID], function (tx, results) {
+
+			// if yes, remove them
+			if(results.rows.item(0).number >= num){
+
+				tx.executeSql("UPDATE inventory SET number = number - ? WHERE itemID = ?", [num, itemID], function (tx, results) {
+
+					crafting.webdb.onSuccess;
+					if(flush){
+						flushScreen();
+					}
+					console.log("update");
+
+				},crafting.webdb.onError);
+			}
+			else{ alert("Cannot remove Item! Not enough left."); }
+
+		},crafting.webdb.onError);
+  });
+};
+
+
+
+/* --------------- Display Items ---------------------- */
+
+crafting.webdb.getAllItems = function() {
+
+	var allItems = document.getElementById("itemlist");
+	allItems.innerHTML = "";
+
+	crafting.webdb.db.transaction(function(tx) {
+		tx.executeSql("SELECT ID FROM item", [], function (tx, results) {
+
+			for(var i=0; i < results.rows.length; i++) {
+				crafting.webdb.showItem(results.rows.item(i).ID, allItems);
+			}
+		},crafting.webdb.onError);
+  });
+};
+
+crafting.webdb.getMyItems = function() {
+
+	var myItems = document.getElementById("inventory");
+	myItems.innerHTML = "";
+
+	crafting.webdb.db.transaction(function(tx) {
+		tx.executeSql("SELECT * FROM item INNER JOIN inventory ON item.ID = inventory.itemID", [], function (tx, results) {
+
+			for(var i=0; i < results.rows.length; i++) {
+				crafting.webdb.showInventoryItem(results.rows.item(i).ID, myItems);
+			}
+		}, crafting.webdb.onError);
+	});
+};
+
+crafting.webdb.getRecipes = function() {
+
+	var recipes = document.getElementById("recipes");
+	recipes.innerHTML = "";
+
+	crafting.webdb.db.transaction(function(tx) {
+		tx.executeSql("SELECT * FROM recipes", [], function (tx, results) {
+
+			for(var i=0; i < results.rows.length; i++) {
+
+				var items = [results.rows.item(i).component1ID, results.rows.item(i).component2ID, results.rows.item(i).component3ID];
+				crafting.webdb.showRecipe(items, results.rows.item(i).resultID, recipes);
+			}
+		},crafting.webdb.onError);
+	});
+};
+
+crafting.webdb.showItem = function (itemID, goal) {
+
+	crafting.webdb.db.transaction(function(tx) {
+		tx.executeSql("SELECT * FROM item WHERE ID = ?", [itemID], function (tx, results) {
+
+			for(var i=0; i < results.rows.length; i++) {
+				goal.innerHTML += renderItem(results.rows.item(i));
+			}
+		},crafting.webdb.onError);
+	});
+};
+
+crafting.webdb.showInventoryItem = function (itemID, goal) {
+
+	crafting.webdb.db.transaction(function(tx) {
+		tx.executeSql("SELECT * FROM item INNER JOIN inventory ON item.ID = inventory.itemID WHERE ID = ?", [itemID], function (tx, results) {
+
+			for(var i=0; i < results.rows.length; i++) {
+				goal.innerHTML += renderInventory(results.rows.item(i));
+			}
+		},crafting.webdb.onError);
+	});
+};
+
+crafting.webdb.showRecipe = function (recipeItems, resultID, goal) {
+
+		var available = 0;
+
+		crafting.webdb.db.transaction(function(tx) {
+
+			for(var i=0; i < recipeItems.length; i++){
+					tx.executeSql("SELECT * FROM item INNER JOIN inventory ON item.ID = inventory.itemID WHERE ID = ?", [recipeItems[i]], function (tx, results) {
+
+						goal.innerHTML += renderIngedient(results.rows.item(0));
+
+						if(results.rows.item(0).number > 0){ available++; }
+
+				},crafting.webdb.onError);
 			}
 
-			recipeHTML += '<li class="' + itemAvaiable +'"><i class="icon-' + itemIcon + ' icon-3x"><span class="num">' + itemNum + '</span><span class="name">' + itemName + '</span></i></li>';
+			tx.executeSql("SELECT * FROM item WHERE ID = ?", [resultID], function (tx, results) {
 
+					goal.innerHTML += '<li><i class="icon-arrow-right"> > </</li>';
+					goal.innerHTML += renderItem(results.rows.item(0));
+
+					if(available === recipeItems.length){
+						goal.innerHTML += "<button id=" + resultID + ">Craft this</button>";
+						$('#'+resultID).click(function(){
+							crafting.webdb.craft(resultID);
+						});
+					}
+
+			},crafting.webdb.onError);
 		});
+};
 
-		var resultId = (recipe["result"]);
-		var resultIcon = inventory[resultId].icon;
-		var resultName = inventory[resultId].name;
-
-		recipeHTML +='<li><i class="icon-arrow-right icon-2x"></i><li class="result"><i class="icon-' + resultIcon + ' icon-3x"><span class="name">' + resultName + '</span></i> ';
-
-		if(craftable){
-			recipeHTML +='</li><button class="available" id="craft' + i + '" > Craft this</button>';
-		}else{
-			recipeHTML +="</li>";
-		}
-
-		$("#recipes").append(recipeHTML);
-
-		$("#craft" + i).click(function(){craft(i);});
-	});
+function renderItem(row) {
+	return '<li><i class="item ' + row.class + ' "><span class="name">' + row.name + '</span></i></li>';
 }
 
-function craft(recipeId){
-
-	var recipe = recipes[recipeId];
-	var feedback;
-	var fade = "";
-
-	addItem(recipe.result);
-
-	feedback = "You crafted a " + inventory[recipe.result].name + ". These Items where removed from your Inventory: ";
-
-	$.each(recipe["needed"], function(i) {
-		var itemId = recipe["needed"][i]["id"];
-		var itemNum = recipe["needed"][i]["num"];
-
-		removeItem(itemId, itemNum);
-
-		feedback += inventory[itemId].name + ", ";
-		//fade += "#inventory .icon-" + inventory[itemId].icon + ", ";
-	});
-
-	//fade = fade.slice(0,-2);
-
-	//$(fade).fadeOut(700, function(){
-
-		listInventory();
-		listRecipes();
-	//});
-	alert(feedback);
-}
-
-function removeItem(itemId, num){
-	if(inventory[itemId].numInInventory>=num){
-		inventory[itemId].numInInventory -= num;
+function renderInventory(row) {
+	if(row.number === 0){
+		return "";
 	}
 	else{
-		alert("error!");
+		return '<li><i class="item ' + row.class + ' "><span class="num">' + row.number + '</span><span class="name">' + row.name + '</span></i></li>';
 	}
 }
 
-function addItem(itemId){
-	inventory[itemId].numInInventory += 1;
-//	listInventory();
-//	listRecipes();
+function renderIngedient(row) {
+	var available;
+
+	if(row.number>0){ available = "available";}
+	else{ available = "notAvailable";}
+
+	return '<li><i class="item ' + row.class + ' "><span class="num ' +available+'">' + row.number + '</span><span class="name">' + row.name + '</span></i></li>';
 }
+
+/* Craft */
+crafting.webdb.craft = function (resultID) {
+
+	crafting.webdb.db.transaction(function(tx) {
+		tx.executeSql("SELECT * FROM recipes WHERE resultID = ?", [resultID], function (tx, results) {
+
+			crafting.webdb.removeFromInventory(results.rows.item(0).component1ID, 1, false);
+			crafting.webdb.removeFromInventory(results.rows.item(0).component2ID, 1, false);
+			crafting.webdb.removeFromInventory(results.rows.item(0).component3ID, 1, false);
+
+			crafting.webdb.addToInventory(results.rows.item(0).resultID, 1, false);
+			flushScreen();
+			console.log("Craft + flush!");
+
+		},crafting.webdb.onError);
+	});
+};
---
+++ source/htdocs/js/craftingData.js
@@ -0,0 +1,38 @@
+/*------------- Data for Items ------------------------------------------------*/
+
+
+/* List Items here:
+			| ID	| Name		   | css class	   | damage min  | damage max  */
+
+var items = [[	1,	'Pipe',			'pipe',			2,				10 ],
+			 [	2,	'Cloth',		'cloth',		1,				2 ],
+			 [	3,	'Stone',		'stone',		2,				6 ],
+			 [	4,	'Barbed Wire',	'wire',			2,				4 ],
+			 [	8,	'Cord',			'cord',			1,				1 ],
+			 [	11,	'Battery',		'battery',		1,				4 ],
+			 [	12,	'Light Bulb',	'light',		1,				2 ],
+			 [	13,	'Flashlight',	'flashlight',	2,				5 ],
+			 [	14,	'Nail',			'nail',			2,				4 ],
+			 [	19,	'Syringe',		'syringe',		1,				5 ]
+			];
+
+
+/* List Inventory here:
+					| Item ID	| number of times in inventory |   */
+ var myItems = [	[	1,			0	],
+					[	2,			5	],
+					[	3,			3	],
+					[	4,			1	],
+					[	8,			1	],
+					[	11,			1	],
+					[	12,			1	],
+					[	13,			0	],
+					[	14,			1	],
+					[	19,			0	]
+				];
+
+
+/* List Recipes here:
+				| Result Item ID | ID component 1  | ID component 2	 | ID component 3 |   */
+ var recipes = [[	13,				11,				12,					8]
+				];
