b0586de564297e38ecb6994040e36748bdea6126
luca.hofmann <luca.hofmann@innogamed.de>
2014-08-08 15:07:56 +0200

    Experiments

---
+++ project/StarCrash/Assets/InputBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)) { 
+
+
+			/*Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);           
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+
+			}
+			Debug.Log(ray.direction);
+			Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green, 100, false);*/
+
+			Fire();
+		}
+	}
+
+	void Fire () {
+
+		float laserLength = 10000;
+
+		Vector3 muzzlePosition = GameObject.Find("Muzzle").transform.position;
+		Vector3 direction = Quaternion.AngleAxis(- 90, Vector3.up) * GameObject.Find("Player").transform.right;
+		Vector3 laserTargetPosition = muzzlePosition + direction * laserLength;
+
+		Ray laserRay = new Ray (muzzlePosition, laserTargetPosition);
+
+		RaycastHit hit;
+		if (Physics.Raycast (laserRay, out hit)) {
+			laserTargetPosition = hit.point;
+		} 
+
+
+
+		Debug.DrawLine(muzzlePosition, laserTargetPosition, Color.blue, 1, true);
+
+	}
+
+
+}
---
+++ project/StarCrash/Assets/LevelBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		/*MeshCollider meshc = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
+
+		foreach (Transform child in gameObject.GetComponentsInChildren<Transform>()) {
+
+			MeshCollider mc = GetComponent<MeshCollider>();
+			myMC.sharedMesh = child.GetComponent<MeshFilter> ().mesh;
+		}*/
+
+		Transform[] allChildren = GetComponentsInChildren<Transform>();
+		foreach (Transform child in allChildren) {
+			MeshCollider mc = GetComponent<MeshCollider>();
+			mc.sharedMesh = child.GetComponent<MeshFilter> ().mesh;
+		}
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
