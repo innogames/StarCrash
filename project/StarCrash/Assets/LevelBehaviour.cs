0dae9941400e3ad070bf4a4b3d43f2620ca4bf51
luca.hofmann <luca.hofmann@innogamed.de>
2014-08-08 15:36:27 +0200

    Added MeshCollider for level objects

--- project/StarCrash/Assets/LevelBehaviour.cs
+++ project/StarCrash/Assets/LevelBehaviour.cs
@@ -2,29 +2,18 @@
 using System.Collections;
 
 public class LevelBehaviour : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
-
-		/*MeshCollider meshc = gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
-
-		foreach (Transform child in gameObject.GetComponentsInChildren<Transform>()) {
-
-			MeshCollider mc = GetComponent<MeshCollider>();
-			myMC.sharedMesh = child.GetComponent<MeshFilter> ().mesh;
-		}*/
-
 		Transform[] allChildren = GetComponentsInChildren<Transform>();
 		foreach (Transform child in allChildren) {
-			MeshCollider mc = GetComponent<MeshCollider>();
-			mc.sharedMesh = child.GetComponent<MeshFilter> ().mesh;
+			MeshCollider mc = child.gameObject.AddComponent(typeof(MeshCollider)) as MeshCollider;
+			mc = new MeshCollider();
 		}
-		
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 }
