d69a382d8a85fe864fe4064fd36cc00398413227
Luca Hofmann <luca.hofmann@innogames.de>
2014-11-14 15:57:53 +0100

    stuff

--- project/StarCrash/Assets/InputBehaviour.cs
+++ project/StarCrash/Assets/InputBehaviour.cs
@@ -20,18 +20,18 @@ public class InputBehaviour : MonoBehaviour {
 			}
 			Debug.Log(ray.direction);
 			Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green, 100, false);*/
-
+			Debug.Log("Fire");
 			Fire();
 		}
 	}
 
 	void Fire () {
 
-		float laserLength = 10000;
+		float laserMaxLength = 10000;
 
 		Vector3 muzzlePosition = GameObject.Find("Muzzle").transform.position;
-		Vector3 direction = Quaternion.AngleAxis(- 90, Vector3.up) * GameObject.Find("Player").transform.right;
-		Vector3 laserTargetPosition = muzzlePosition + direction * laserLength;
+		Vector3 direction = Quaternion.AngleAxis(- 90, Vector3.up) * GameObject.Find("Aim").transform.right;
+		Vector3 laserTargetPosition = muzzlePosition + direction * laserMaxLength;
 
 		Ray laserRay = new Ray (muzzlePosition, laserTargetPosition);
 
@@ -39,10 +39,24 @@ public class InputBehaviour : MonoBehaviour {
 		if (Physics.Raycast (laserRay, out hit)) {
 			laserTargetPosition = hit.point;
 		} 
+		LineRenderer beam = GameObject.Find ("LaserBeam").GetComponent<LineRenderer> ();
+		LaserBeamBehaviour beamBehaviour = GameObject.Find ("LaserBeam").GetComponent<LaserBeamBehaviour> ();
 
 
 
-		Debug.DrawLine(muzzlePosition, laserTargetPosition, Color.blue, 0.1f, true);
+
+		beamBehaviour.Fire (muzzlePosition, laserTargetPosition);
+
+		beam.SetPosition (0, muzzlePosition);
+		beam.SetPosition (1, laserTargetPosition);
+
+		GameObject impact = GameObject.Find ("Impact");
+		impact.transform.position = laserTargetPosition;
+		Detonator detonator = impact.GetComponent<Detonator> ();
+		detonator.direction = laserTargetPosition;
+		detonator.Explode ();
+
+
 
 	}
 }
---
+++ project/StarCrash/Assets/LaserBeamBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBeamBehaviour : MonoBehaviour {
+
+	private float aplha = 1.0f;
+	Light light;
+	Light lightImpact;
+
+	// Use this for initialization
+	void Start () {
+		light = GameObject.Find ("MuzzleLight").GetComponent<Light> ();
+		lightImpact = GameObject.Find ("ImpactLight").GetComponent<Light> ();
+	}
+
+	public void Fire(Vector3 start, Vector3 end) {
+		light.transform.position = start;
+		light.intensity = 1;
+
+		lightImpact.transform.position = end;
+		lightImpact.intensity = 1;
+
+		LineRenderer line = GetComponent<LineRenderer> ();
+		Color color = line.material.GetColor("_TintColor");
+		color.a = 1.0f;
+		line.material.SetColor("_TintColor", color);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		LineRenderer line = GetComponent<LineRenderer> ();
+		Color color = line.material.GetColor("_TintColor");
+		color.a -= 0.3f;
+		light.intensity -= 0.5f;
+		lightImpact.intensity -= 0.5f;
+		line.material.SetColor("_TintColor", color);
+	}
+
+
+
+}
